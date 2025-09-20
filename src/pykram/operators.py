9c415fa89facf385f18554c32034b9cba82921e4
developer.aayas <developer.aayas@ayas-MacBook-Air.local>
2025-09-20 11:55:07 +0530

    added the logic to parse outputfields and stored those in workflow context

--- src/pykram/operators.py
+++ src/pykram/operators.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from types import MethodType
 from .dag import Workflow
 from .utils import run_executable_with_parameters
+from .types import OutputField
 
 class ExecutionRuntime:
     '''
@@ -32,8 +33,32 @@ class ExecutionRuntime:
                 
     def execute_(self, task_class):
         task_instance = task_class()
-        run_executable_with_parameters(
+        response = run_executable_with_parameters(
             task_instance, self.workflow_instance.workflow_context)
+        type_of_response = type(response)
+
+        if type_of_response is not tuple:
+            response = (response,)
+        # Now parse the output fields and store the returned response accordingly
+        output_fields = []
+        for index, field_name, field_definition in enumerate(task_instance.__dict__.items()):
+            if field_definition.__class__ == OutputField:
+                 
+                value_to_be_stored = response[index]
+                # validate the datatype of the output if mentioned in the declaration
+                if field_definition.validate_datatype:
+                    if type(value_to_be_stored) != field_definition.datatype:
+                        raise Exception(f"Wrong datatype provided for field {field_name}")
+                
+                self.workflow_instance.workflow_context[field_name] = value_to_be_stored
+    
+        
+        
+
+        
+
+
+
         
 
 
